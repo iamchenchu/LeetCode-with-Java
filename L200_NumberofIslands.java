1c32aa0a98b85c92a3ffbab4de2a828e1c31f84b
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-12-27 20:03:55 -0600

    Added code for MaxSubArray using kadans algorithm

--- L200_NumberofIslands.java
+++ L200_NumberofIslands.java
@@ -6,28 +6,23 @@
 //   ["1","1","0","1","0"],
 //   ["1","1","0","0","0"],
 //   ["0","0","0","0","0"]
-
 class L200_NumberofIslands {
   void dfs(char[][] grid, int r, int c) {
     int nr = grid.length;
     int nc = grid[0].length;
-
     if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
       return;
     }
-
     grid[r][c] = '0';
     dfs(grid, r - 1, c);
     dfs(grid, r + 1, c);
     dfs(grid, r, c - 1);
     dfs(grid, r, c + 1);
   }
-
   public int numIslands(char[][] grid) {
     if (grid == null || grid.length == 0) {
       return 0;
     }
-
     int nr = grid.length;
     int nc = grid[0].length;
     int num_islands = 0;
@@ -39,7 +34,6 @@ class L200_NumberofIslands {
         }
       }
     }
-
     return num_islands;
   }
 }
